bba4950b3196ee81672f6455fe9820739ca6f8ef
Ridwan <belloridwan60@gmail.com>
2025-07-18 17:10:44 -0500

    Fix: corrected error in the path, glob, and parent stem

--- data_setup/accentdb_data_setup.py
+++ data_setup/accentdb_data_setup.py
@@ -14,15 +14,13 @@ def download_accentdb(accent_list: List) -> Tuple[List, List]:
         Returns:
             A tuple of audio and label
             A print of:
-                Information about the number of files in folders and subfolders
-
-        
+                Information about the number of files in folders and subfolders 
     """
     def check_if_its_the_right_input_format(data):
         return isinstance(data, list) and all(isinstance(item, str) for item in data)
     
     if(check_if_its_the_right_input_format(accent_list) == False):
-        return f"Data not in the right format e.g {["american", "indian"]}"
+        return f"Data not in the right format. Shoul be like ['american', 'indian']"
     
     try:
         import gdown
@@ -51,17 +49,16 @@ def download_accentdb(accent_list: List) -> Tuple[List, List]:
 
     for dirpath, dirnames, filenames in os.walk(audio_data):
         print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
-    source_dir = audio_data
-    destination_dir = data_path / "audio_datas"
-    os.makedirs(destination_dir, exist_ok=True)
-    # Loop through each folder and copy it
+    accent_data = data_path / "accent_data"
+    os.makedirs(accent_data, exist_ok=True)
+    # Move folders in selected accented to a new folder called "accent_data"
     for folder in accent_list:
-        src_path = os.path.join(source_dir, folder)
-        dest_path = os.path.join(destination_dir, folder)
+        src_path = os.path.join(audio_data, folder)
+        dest_path = os.path.join(accent_data, folder)
 
         if os.path.exists(src_path):
             shutil.copytree(src_path, dest_path)
-            print(f"Copied: {folder}")
+            print(f"Moved : {folder}")
         else:
             print(f"Folder not found: {folder}")
     
@@ -75,41 +72,39 @@ def download_accentdb(accent_list: List) -> Tuple[List, List]:
             else:
                 print("Folder does not exist.")
     
-    american_folders_to_delete = Path(destination_dir / "american")
+    american_folders_to_delete = Path(accent_data / "american")
     delete_american_folders(american_folders_to_delete)
 
-    # Move each accent class folder to a parent folder for the classification format
-    def move_folders_to_parent(folders_to_move: List, new_parent_folder: str):
-        """ Create the new parent folder if not exist and moves all specified folders to the newly created folder.
+    # # Move each accent class folder to a parent folder for the classification format
+    # def move_folders_to_parent(folders_to_move: List, new_parent_folder: str):
+    #     """ Create the new parent folder if not exist and moves all specified folders to the newly created folder.
         
-        Args:
-        folders_to_move: An array consisting of all the accent folder paths to be moved to the parent folder
-        new_parent_folder: Name of the new parent folder to be created
+    #     Args:
+    #     folders_to_move: An array consisting of all the accent folder paths to be moved to the parent folder
+    #     new_parent_folder: Name of the new parent folder to be created
 
-        Returns:
-        A message showing the successful moving of the folders
+    #     Returns:
+    #     A message showing the successful moving of the folders
         
-        Raise:
-        Exception: If there is any error while moving the folders
-        """
-        os.makedirs(new_parent_folder, exist_ok=True)
+    #     Raise:
+    #     Exception: If there is any error while moving the folders
+    #     """
+    #     os.makedirs(new_parent_folder, exist_ok=True)
 
-        for folder in folders_to_move:
-            try:
-                folder_name = os.path.basename(folder)
-                destination = os.path.join(new_parent_folder, folder_name)
-                shutil.move(folder, destination)
-            except Exception as e:
-                print(f"Error moving the folders {e}")
-        print("Folders moved successfully")
+    #     for folder in folders_to_move:
+    #         try:
+    #             folder_name = os.path.basename(folder)
+    #             destination = os.path.join(new_parent_folder, folder_name)
+    #             shutil.move(folder, destination)
+    #         except Exception as e:
+    #             print(f"Error moving the folders {e}")
+    #     print("Folders moved successfully")
     
-    parent_folder = "accent_db"
-    move_folders_to_parent(accent_list, parent_folder)
+    # parent_folder = "accent_db"
+    # move_folders_to_parent(accent_list, parent_folder)
 
     # Prepare and return audio list
-    audio_files = list(parent_folder.glob("*/*.wav"))
-    data = [(file, file.parent.stem) for file in audio_files]
+    audio_files = list(data_path.glob("*/*/*.wav"))
+    data = [(file, file.parent.parent.stem) for file in audio_files]
     audio_list, label_list = zip(*data)
     return audio_list, label_list
-
-
